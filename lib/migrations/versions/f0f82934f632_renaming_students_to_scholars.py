30d04ad75858167190cae055f0451e18dfdddbba
Pauline Moraa <pmoraa431@gmail.com>
2025-05-25 14:34:26 +0300

    manual migrations and downgrades

---
+++ lib/migrations/versions/f0f82934f632_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: f0f82934f632
+Revises: 791279dd0760
+Create Date: 2025-05-25 14:20:47.412927
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f0f82934f632'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
